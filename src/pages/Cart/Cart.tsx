c6273f7638f27b2ae1aff15574afb7c0c9150a19
pelumini <pelumini@gmail.com>
2022-08-11 07:57:31 +0100

    first commit

---
+++ src/pages/Cart/Cart.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface CartProps {
+  className?: string;
+}
+
+export const Cart: React.FC<CartProps> = ({ className }): JSX.Element => {
+  return <div>Cart</div>;
+};
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface HomeProps {
+  className?: string;
+}
+
+export const Home: React.FC<HomeProps> = ({ className }): JSX.Element => {
+  return <div>Home</div>;
+};
